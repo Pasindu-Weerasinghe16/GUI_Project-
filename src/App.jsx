2f44af8a7c9b94865fce28d74c32c34f4c4bac15
Pasindu Weerasinghe <pasinduweerasinghe69@gmail.com>
2024-12-31 00:01:17 +0530

    Change_02

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar.jsx";
 import HeroSection from "./components/HeroSection/HeroSection.jsx";
+import SearchBar from "./components/SearchBar/SearchBar.jsx";
 
 function App() {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <SearchBar />
     </>
   );
 }
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./components/SearchBar/SearchBar.css";
+import "./SearchBar.css";
 
 const SearchBar = () => {
     return (
